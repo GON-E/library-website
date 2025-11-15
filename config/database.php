a24a3914cdc58a93fb2c6d1c31e9384111b39fba
Justin Gonzaga <justingonzaga333@gmail.com>
2025-11-15 13:48:39 +0800

    First Commit

---
+++ config/database.php
@@ -0,0 +1,15 @@
+<?php  
+  $db_server = "localhost"; // The Database Server
+  $db_user = "root"; // default MySQL username
+  $db_pass = ""; // default MySQL password
+  $db_name = "librarysysdb"; // Name of the Database  
+
+  $conn = "";
+
+  try {
+    $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
+    echo "Database Connected! <br>";
+  } catch(mysqli_sql_exception) {
+    echo "Could not Connect! <br>";
+  }
+?>
---
+++ pages/sign-up.php
@@ -0,0 +1,59 @@
+<?php 
+  include("../config/database.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <!-- Sign-Up Form -->
+  <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"])?> " method="post">
+    username:
+    <input type="text" name="username"> <br>
+    password:
+    <input type="password" name="password"> <br>
+    <input type="submit" name="submit" value="Register" >
+  </form>
+  <!-- End of Sign-Up Form-->
+</body>
+</html>
+
+<?php 
+  if($_SERVER["REQUEST_METHOD"] == "POST") { // Check if the method is post if yes
+    // Variable for admin
+    $admin = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
+    // Variable for password
+    $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_SPECIAL_CHARS);
+    $pattern = '/^[a-zA-Z]*$/';
+
+    if(preg_match($pattern, $admin)){
+      echo "Username is Valid <br>";
+    } else {
+      echo "Username is Invalid! Please enter only characters (no numbers). <br>";
+    }
+
+    if(empty($admin)){  // If admin is empty
+      echo "Username is Missing!";
+    } else if (empty($password)) { // If password is empty
+      echo "Password is Missing!";  
+    } else { // Else hash the password for security
+        $hash = password_hash($password, PASSWORD_DEFAULT); // hash
+
+        // SQL QUERY Insert data in the database
+        $sql = "INSERT INTO admins (admin, password) 
+        VALUES ('$admin', '$hash')";  
+
+      try { // Try query  
+        mysqli_query($conn, $sql);
+        echo "You are now registered!";
+      } catch(mysqli_sql_exception) { // Catch Error
+        echo "Username is already taken!";
+      }
+    }
+  }
+  mysqli_close($conn);
+?>  
